548e0c2857d1089bedf18522b4b1e6f3fbf0809a
NEONKID <contact@neonkid.xyz>
2022-01-05 22:48:33 +0900

    Mod dto class..

--- core/src/main/kotlin/xyz/neonkid/cms/modules/post/useCases/queries/dto/PostDTO.kt
+++ core/src/main/kotlin/xyz/neonkid/cms/modules/post/useCases/queries/dto/PostDTO.kt
@@ -1,4 +1,4 @@
-package xyz.neonkid.cms.modules.post.useCases.queries.dto
+package xyz.neonkid.cms.modules.post.useCases.queries.dtos
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
