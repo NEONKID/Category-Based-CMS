4614e8c54f95f2ec35e526e7647da629c4509399
NEONKID <contact@neonkid.xyz>
2022-01-09 16:13:43 +0900

    Add before save event callback..

--- core/src/main/kotlin/xyz/neonkid/cms/core/CoreConfig.kt
+++ core/src/main/kotlin/xyz/neonkid/cms/core/CoreConfig.kt
@@ -2,7 +2,10 @@ package xyz.neonkid.cms.core
 
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
+import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories
+import xyz.neonkid.cms.core.jdbc.JdbcConfig
 
 /**
  * Created by Neon K.I.D on 12/31/21
@@ -11,5 +14,7 @@ import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories
  */
 @Configuration
 @ComponentScan(basePackages = ["xyz.neonkid.cms.modules"])
+@EnableJdbcAuditing
 @EnableJdbcRepositories(value = ["xyz.neonkid.cms.core", "xyz.neonkid.cms.persistence"])
+@Import(JdbcConfig::class)
 class CoreConfig
---
+++ core/src/main/kotlin/xyz/neonkid/cms/core/jdbc/JdbcConfig.kt
@@ -0,0 +1,17 @@
+package xyz.neonkid.cms.core.jdbc
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration
+import xyz.neonkid.cms.persistence.author.BeforeSaveAuthorCallback
+
+/**
+ * Created by Neon K.I.D on 1/8/22
+ * Blog : https://blog.neonkid.xyz
+ * Github : https://github.com/NEONKID
+ */
+@Configuration
+class JdbcConfig : AbstractJdbcConfiguration() {
+    @Bean
+    fun beforeSaveAuthorCallback() = BeforeSaveAuthorCallback()
+}
